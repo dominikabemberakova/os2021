bbdea509384faf2a4c6c0bde84edb38e26c3a216
Dominika Bemberakova <dominika.bemberakova@gmai.com>
2021-10-17 14:08:54 +0200

    feat: implemented Speed up system calls

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,5 +104,6 @@ struct proc {
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
+  struct usyscall *usyscall;    // shared with kernel
   char name[16];               // Process name (debugging)
-};
+  };
